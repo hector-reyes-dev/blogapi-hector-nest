68b88eda6f6b27fabd9c7c225b321e07b8f30c5a
Hector <hectordevx@gmail.com>
2024-01-23 13:58:16 -0600

    feat(config): Railway config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -29,6 +30,6 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
